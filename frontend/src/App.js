77411ecaa887940b4042f19fc2d73b2e906b1024
kaaado <yacineyoyoker@gmail.com>
2025-02-19 15:27:10 +0100

    Add Home & configure Toast Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import {  Outlet } from "react-router-dom";
-
+import toast from 'react-hot-toast';
+import CustomToaster from "./Components/CustomToast";
 
 function App() {
 
   return (
     <>
+  	 <CustomToaster />
      <Outlet />
     </>
   );
