f86eb99add9c8669e5826c5b5c2b4efc2a1e928e
kaaado <yacineyoyoker@gmail.com>
2025-02-20 21:06:53 +0100

    Update Routes in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,13 +22,21 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
-});
+    Route::post('/passowrd', 'sendResetLink');
 
-// Protected Routes
-Route::middleware('auth:api')->group(function () {
-    Route::get('/user', [UsersContoller::class, 'authUser']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::post('/reset-password', 'reset');
 });
 
 Route::get('/login-google', [socialAuthController::class, 'redirectToProvider']);
 Route::get('/auth/google/callback', [socialAuthController::class, 'handleCallback']);
+
+// Protected Routes
+Route::middleware('auth:api')->group(function () {
+    // Users
+    Route::controller(UsersContoller::class)->group(function () {
+        Route::get('/user', 'authUser');
+    });
+
+    // Auth
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
